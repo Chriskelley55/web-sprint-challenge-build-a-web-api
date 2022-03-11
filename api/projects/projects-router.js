01225125f84f54287b3d4c7fd7d73a1bb6bd5868
Chris Kelley <chriskelley85@gmail.com>
2022-03-11 13:37:53 -0600

    server running, middleware up

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const { validateUserId, validatePost, allComplete, } = require('./projects-middleware');
+const Project = require('./projects-model');
+
+router.get('/', async (req, res, next) => {
+    try {
+        const project = await Project.get()
+        res.status(200).json(project)
+    } catch(err){
+        next(err)
+    }
+})
+
+module.exports = router
