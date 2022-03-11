0a69f6517015f62052ab15b6856ad9809a694b93
Chris Kelley <chriskelley85@gmail.com>
2022-03-11 14:05:05 -0600

    action routers setup

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -12,41 +12,40 @@ router.get('/', async (req, res, next) => {
         next(err)
     }
 });
+router.get('/:id', validateUserId, async (req, res, next) => {
+    try {
+        const action = await Action.get(req.action.id)
+        res.status(200).json(action)  
+    } catch(err){
+        next(err)
+    }
+})
 
-// router.get('/:id', validateUserId, async (req, res, next) => {
-//     try {
-//         const action = await Action.get(req.action.id)
-//         res.status(200).json(action)  
-//     } catch (err){
-//         next(err)
-//     }
-// })
+router.post('/', validatePost, async (req, res, next) => {
+    try {
+        const newAction = await Action.insert(req.body)
+            res.status(200).json(newAction)
+    } catch(err) {
+        next(err)
+    }
+})
 
-// router.post('/', validatePost, async (req, res, next) => {
-//     try {
-//         const newAction = await Action.insert(req.body)
-//             res.status(200).json(newAction)
-//     } catch (err) {
-//         next(err)
-//     }
-// })
+router.put('/:id', validateUserId, validatePost, allComplete, async (req, res, next) => {
+    try {
+        const updatedAction = await Action.update(req.action.id, req.body)
+        res.status(200).json(updatedAction) 
+    } catch(err) {
+        next(err)
+    }
+})
 
-// router.put('/:id', validateUserId, validatePost, allComplete, async (req, res, next) => {
-//     try {
-//         const updatedAction = await Action.update(req.action.id, req.body)
-//         res.status(200).json(updatedAction) 
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// router.delete('/:id', validateUserId, async (req, res, next) => {
-//     try {
-//         await Action.remove(req.action.id)
-//         res.status(200).json({ message: 'Action successful'})
-//     } catch (err) {
-//         next(err)
-//     }
-// })
+router.delete('/:id', validateUserId, async (req, res, next) => {
+    try {
+        await Action.remove(req.action.id)
+        res.status(200).json({ message: 'Action successful'})
+    } catch(err) {
+        next(err)
+    }
+})
 
 module.exports = router;
